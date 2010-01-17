e508c07da5c6875fcda4e1104e76d30d9a4aced1
Zeng Ke <superisaac.ke@gmail.com>
2010-01-17 13:43:34 +0800

    Added gitignore

--- src/local_settings.py
+++
@@ -1,8 +0,0 @@
-
-NEWS_USER = 'news@example.com'
-NEWS_PASSWORD = 'halihalinews'
-
-# Google map key
-# Please apply it through http://code.google.com/apis/maps/signup.html
-MAP_KEY = 'ABQIAAAADsnbaFl-RI5O69x2LuN9JhRIqEmBdYeQiVoecy_u8nwJh2CJchT_L4HyqzqD_8kDEddc0K0N-JCvHw' # for http://status-engine.appspot.com
-
